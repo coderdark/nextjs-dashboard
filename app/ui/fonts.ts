04e8e08c98051cb18db66a1a61634e8fd4dcf7c3
developer <coderdarkllc@gmail.com>
2024-02-10 22:57:53 -0600

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter, Lusitana, Roboto, Six_Caps} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({weight: ["400", "700"], subsets: ['latin']})
+export const roboto = Roboto({weight: ["100", "300", "400", "700"], subsets: ['latin']})
+export const sixcaps = Six_Caps({weight: '400', subsets: ['latin']})
